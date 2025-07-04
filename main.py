95bc2f52878cd8edd36e0622965499661446d35b
Alan Carey <32581501+alancarey1975@users.noreply.github.com>
2025-07-04 15:18:02 +0100

    move get_num_words function from main.py to stats.py update .gitignore to include *.pyc

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
+from stats import get_num_words
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
-
-def get_num_words(book_text):
-    word_list = book_text.split()
-    num_words = len(word_list)
-    return num_words
 
 def main():
     book_text = get_book_text('./books/frankenstein.txt')
---
+++ stats.py
@@ -0,0 +1,4 @@
+def get_num_words(book_text):
+    word_list = book_text.split()
+    num_words = len(word_list)
+    return num_words
