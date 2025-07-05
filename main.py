fa9e0486c89db7883d9bca5e2117bae85d547312
Alan Carey <32581501+alancarey1975@users.noreply.github.com>
2025-07-05 19:17:35 +0100

    Add code to print report if character alpha. Tidy variables

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_num_words
 from stats import get_num_chars
+from stats import get_sorted_dict
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -7,10 +8,22 @@ def get_book_text(path_to_file):
     return file_contents
 
 def main():
-    book_text = get_book_text('./books/frankenstein.txt')
-    print(f"{get_num_words(book_text)} words found in the document")
-
-    print(f"{get_num_chars(book_text)}")
-
+    book_link = 'books/frankenstein.txt'
+    book_text = get_book_text(book_link)
+    book_nums = get_num_words(book_text)
+    char_dict = get_num_chars(book_text)
+    sorted_dict = get_sorted_dict(char_dict)
+    
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_link}...")
+    print("----------- Word Count ----------")
+    print(f"Found {book_nums} total words")
+    print("--------- Character Count -------")
+    for char in sorted_dict:
+        if char.isalpha():
+            amount = sorted_dict[char]
+            print(f"{char}: {amount}")
+    print("============= END ===============")
 
 main()
